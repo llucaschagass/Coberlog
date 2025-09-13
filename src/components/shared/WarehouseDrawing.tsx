fae952ad175097d37a27e6a8d5807a7e61679b74
MarcoTSF <mtsf.salvador@gmail.com>
2025-09-13 19:00:26 -0300

    style: troca da palette.ts para a palette.css

--- src/components/shared/WarehouseDrawing.tsx
+++ src/components/shared/WarehouseDrawing.tsx
@@ -2,7 +2,7 @@
 
 import { motion, type Variants } from "framer-motion"
 import React from "react"
-import { palette } from "../../assets/styles/palette"
+import '../../assets/styles/palette.css'
 
 const draw: Variants = {
     hidden: { pathLength: 0, opacity: 0 },
@@ -37,138 +37,174 @@ export default function WarehouseDrawing({ width = 800, height = 500 }: Warehous
             {/* Base */}
             <motion.line
                 x1="150" y1="400" x2="650" y2="400"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={1}
-                style={shape}
             />
             {/* Parede Esquerda */}
             <motion.line
                 x1="150" y1="400" x2="150" y2="250"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={1.5}
-                style={shape}
             />
             {/* Parede Direita */}
             <motion.line
                 x1="650" y1="400" x2="650" y2="250"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={1.5}
-                style={shape}
             />
             {/* Telhado Esquerdo */}
             <motion.line
                 x1="150" y1="250" x2="400" y2="150"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2}
-                style={shape}
             />
             {/* Telhado Direito */}
             <motion.line
                 x1="650" y1="250" x2="400" y2="150"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2}
-                style={shape}
             />
             {/* Conexão inferior direita (parede) */}
             <motion.line
                 x1="650" y1="400" x2="750" y2="350"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={1.2}
-                style={shape}
             />
             {/* Conexão superior direita (parede) */}
             <motion.line
                 x1="650" y1="250" x2="750" y2="200"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2.2}
-                style={shape}
             />
             {/* Conexão superior esquerda (parede)*/}
             <motion.line
                 x1="150" y1="250" x2="250" y2="200"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2.2}
-                style={shape}
             />
             {/* Viga mestre do telhado (profundidade) */}
             <motion.line
                 x1="400" y1="150" x2="500" y2="100"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2.4}
-                style={shape}
             />
             <motion.line
                 x1="750" y1="350" x2="750" y2="200"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2.6}
-                style={shape}
             />
             <motion.line
                 x1="250" y1="200" x2="500" y2="100"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2.8}
-                style={shape}
             />
             <motion.line
                 x1="750" y1="200" x2="500" y2="100"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={2.8}
-                style={shape}
             />
             <motion.line
                 x1="250" y1="400" x2="250" y2="210"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={3.5}
-                style={shape}
             />
             <motion.line
                 x1="350" y1="400" x2="350" y2="170"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={3.7}
-                style={shape}
             />
             <motion.line
                 x1="450" y1="400" x2="450" y2="170"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={3.7}
-                style={shape}
             />
             <motion.line
                 x1="550" y1="400" x2="550" y2="210"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={3.5}
-                style={shape}
             />
             {/* Vigas do telhado */}
             <motion.line
                 x1="275" y1="200" x2="375" y2="150"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={4}
-                style={shape}
             />
             <motion.line
                 x1="525" y1="200" x2="625" y2="150"
-                stroke={palette.primary}
+                style={{ 
+                    ...shape, 
+                    stroke: 'var(--color-primary)' 
+                }}
                 variants={draw}
                 custom={4}
-                style={shape}
             />
         </motion.svg>
     )
