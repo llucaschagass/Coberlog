b32107f27a91b0f81b292fbe93a25387a5721979
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2025-09-14 10:35:23 -0300

    fix: removendo estilização não utilizada

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -4,7 +4,6 @@ const HomePage: React.FC = () => {
   return (
     <div style={{ padding: '40px', color: '#fff', textAlign: 'center' }}>
       <h1>Bem-vindo à COBERLOG</h1>
-        <p>Soluções logísticas inovadoras e sustentáveis.</p>
     </div>
   );
 };
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,6 +1,4 @@
-// components/shared/Header/Header.tsx
-
-import { Link } from 'react-router-dom'; // 1. IMPORTE O LINK
+import { Link } from 'react-router-dom';
 import logo from '../../../assets/images/logo-png-branca.png';
 import styles from './Header.module.css';
 
@@ -9,7 +7,6 @@ export default function Header() {
     <header>
       <div className={styles.headerContent}>
         <nav>
-          {/* 2. SUBSTITUA 'a href' POR 'Link to' */}
           <Link to="/">Home</Link>
           <Link to="/produtos">Produtos</Link>
           <Link to="/segmentos">Segmentos</Link>
